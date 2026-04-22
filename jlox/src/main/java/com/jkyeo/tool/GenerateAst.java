97a215dbcab3fa039028475422e553789a50bc4d
Yeo Jing Kang <yeojingkang@gmail.com>
2026-04-23 00:00:07 +0800

    [jlox] Add tool to generate AST node types

---
+++ jlox/src/main/java/com/jkyeo/tool/GenerateAst.java
@@ -0,0 +1,77 @@
+package com.jkyeo.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usages: generate_ast <output_directory>");
+            System.exit(64);
+        }
+
+        final var outputDir = args[0];
+
+        defineAst(outputDir, "Expr", Map.ofEntries(
+                Map.entry("Binary", Arrays.asList(
+                        "Expr left",
+                        "Token operator",
+                        "Expr right"
+                )),
+                Map.entry("Grouping", List.of(
+                        "Expr expression"
+                )),
+                Map.entry("Literal", List.of(
+                        "Object value"
+                )),
+                Map.entry("Unary", Arrays.asList(
+                        "Token operator",
+                        "Expr right"
+                ))
+        ));
+    }
+
+    private static void defineAst(String outputDir, String baseName, Map<String, List<String>> types) throws IOException {
+        final var path = outputDir + "/" + baseName + ".java";
+        final var writer = new PrintWriter(path, StandardCharsets.UTF_8);
+
+        writer.println("package com.jkyeo.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + "{");
+
+        for (final var type : types.entrySet()) {
+            final var className = type.getKey();
+            final var fields = type.getValue();
+            defineType(writer, baseName, className, fields);
+            writer.println();
+        }
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, List<String> fields) {
+        writer.println("  static class " + className + " extends " + baseName + " {");
+
+        // Constructor
+        writer.println("    " + className + "(" + String.join(", ", fields) + ") {");
+        for (final var field : fields) {
+            final var name = field.split(" ")[1].trim();
+            writer.println("      this." + name + " = " + name + ";");
+        }
+        writer.println("    }");
+
+        // Fields
+        writer.println();
+        for (final var field : fields)
+            writer.println("    final " + field + ";");
+
+        writer.println("  }");
+    }
+}
