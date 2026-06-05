9bb9873b70a0273a1f8ba745e0013dbd4fd4b796
Yeo Jing Kang <yeojingkang@gmail.com>
2026-06-06 00:53:50 +0800

    [clox] Constant opcode and line numbers

--- clox/debug.c
+++ clox/debug.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "debug.h"
+#include "value.h"
 
 void disassembleChunk(Chunk* chunk, const char* name) {
     printf("== %s ==\n", name);
@@ -13,11 +14,25 @@ static int simpleInstruction(const char* name, int offset) {
     return offset + 1;
 }
 
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+    const uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
+}
+
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
+    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset-1])
+        printf("   | ");
+    else
+        printf("%4d ", chunk->lines[offset]);
 
     const uint8_t insn = chunk->code[offset];
     switch (insn) {
+    case OP_CONSTANT:
+        return constantInstruction("OP_CONSTANT", chunk, offset);
     case OP_RETURN:
         return simpleInstruction("OP_RETURN", offset);
     default:
--- clox/main.c
+++ clox/main.c
@@ -6,7 +6,11 @@ int main(int argc, const char** argv) {
     Chunk chunk;
     initChunk(&chunk);
 
-    writeChunk(&chunk, OP_RETURN);
+    int constant = addConstant(&chunk, 3.14);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+
+    writeChunk(&chunk, OP_RETURN, 1);
     disassembleChunk(&chunk, "test chunk");
 
     freeChunk(&chunk);
